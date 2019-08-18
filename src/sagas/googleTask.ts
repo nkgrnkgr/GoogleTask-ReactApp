4b1982bf14c5e499838c3e83afdfe49a11c0581a
nkgrnkgr <beefupme@gmail.com>
2019-08-19 03:31:31 +0900

    update insert Task

--- src/sagas/googleTask.ts
+++ src/sagas/googleTask.ts
@@ -3,9 +3,12 @@ import { all, call, fork, put, takeLatest } from 'redux-saga/effects';
 import * as TaskListAction from '../actions/TaskListConstants';
 import * as TaskAction from '../actions/TaskConstants';
 import { getTaskLists } from '../actions/TaskList';
-import { getTaskList } from '../actions/Task';
+import { getTaskList, insertTask } from '../actions/Task';
 import { getTaskListsFactory } from '../services/googleTasks/taskListApi';
-import { getTaskListFactory } from '../services/googleTasks/taskApi';
+import {
+  getTaskListFactory,
+  insertTaskFactory,
+} from '../services/googleTasks/taskApi';
 
 function* runGetTaskLists() {
   try {
@@ -37,6 +40,28 @@ export function* watchGetTaskList() {
   yield takeLatest(TaskAction.GET_TASKLIST_START, runGetTaskList);
 }
 
+function* runInsertTask(action: ReturnType<typeof insertTask.start>) {
+  const { tasklist, title, parent, previous } = action.payload;
+
+  try {
+    const api = insertTaskFactory();
+    const task = yield call(api, { tasklist, title, parent, previous });
+
+    yield put(
+      insertTask.succeed({ tasklist, title, parent, previous }, { task }),
+    );
+  } catch (error) {
+    yield put(insertTask.fail({ tasklist, title, parent, previous }, error));
+  }
+}
+export function* watchInsertTask() {
+  yield takeLatest(TaskAction.INSERT_TASK_START, runInsertTask);
+}
+
 export default function* rootSaga() {
-  yield all([fork(watchGetTaskLists), fork(watchGetTaskList)]);
+  yield all([
+    fork(watchGetTaskLists),
+    fork(watchGetTaskList),
+    fork(watchInsertTask),
+  ]);
 }
