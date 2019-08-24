7f175e3741bb4a67f8a5b86cda311c12c9f53767
nkgrnkgr <beefupme@gmail.com>
2019-08-24 23:17:24 +0900

    update drag&drop

--- src/comtainers/TaskListContainer/index.tsx
+++ src/comtainers/TaskListContainer/index.tsx
@@ -2,13 +2,19 @@ import React, { FC, useEffect } from 'react';
 import { bindActionCreators, Dispatch } from 'redux';
 import { connect } from 'react-redux';
 import { Task } from '../../services/googleTasks/models';
-import { getTaskList, patchTask, deleteTask } from '../../actions/Task';
+import {
+  getTaskList,
+  patchTask,
+  deleteTask,
+  moveTask,
+} from '../../actions/Task';
 import TaskListContainer from '../../components/TaskListContainer';
 import { CombinedState } from '../../reducers/root';
 import {
   TasksListParam,
   TasksPatchParam,
   TasksDeleteParam,
+  TasksMoveParam,
 } from '../../services/googleTasks/taskApi';
 
 interface StateProps {
@@ -21,6 +27,7 @@ interface DispatchProps {
   getTaskListStart: (param: TasksListParam) => void;
   patchTaskStart: (param: TasksPatchParam) => void;
   deleteTaskStart: (param: TasksDeleteParam) => void;
+  moveTaskStart: (param: TasksMoveParam) => void;
 }
 
 type EnhancemembersProps = StateProps & DispatchProps;
@@ -37,6 +44,7 @@ const mapDispatchToProps = (dispatch: Dispatch): DispatchProps =>
       getTaskListStart: (param: TasksListParam) => getTaskList.start(param),
       patchTaskStart: (param: TasksPatchParam) => patchTask.start(param),
       deleteTaskStart: (param: TasksDeleteParam) => deleteTask.start(param),
+      moveTaskStart: (param: TasksMoveParam) => moveTask.start(param),
     },
     dispatch,
   );
@@ -45,6 +53,7 @@ const TaskListContainerContainer: FC<EnhancemembersProps> = ({
   getTaskListStart,
   patchTaskStart,
   deleteTaskStart,
+  moveTaskStart,
   taskList,
   isLoading = false,
   selectedTaskListId,
@@ -68,12 +77,28 @@ const TaskListContainerContainer: FC<EnhancemembersProps> = ({
     deleteTaskStart({ tasklist: selectedTaskListId, task: task.id || '' });
   };
 
+  const handleReorderTask = (sourceIndex: number, destinationIndex: number) => {
+    const sourceTaskId = taskList[sourceIndex].id || '';
+    const destinationTaskId = taskList[destinationIndex].id || '';
+
+    if (sourceTaskId === '' || destinationTaskId === '') {
+      return;
+    }
+
+    moveTaskStart({
+      tasklist: selectedTaskListId,
+      task: sourceTaskId,
+      previous: destinationTaskId,
+    });
+  };
+
   return (
     <TaskListContainer
       taskList={taskList}
       isLoading={isLoading}
       handleOnChange={handleOnChangeTask}
       handleDelete={handleDeleteTask}
+      handleReorder={handleReorderTask}
     />
   );
 };
