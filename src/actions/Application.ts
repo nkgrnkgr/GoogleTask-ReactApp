944ba2e06ed870a93009f92e25a7c9f54f88eaea
nkgrnkgr <beefupme@gmail.com>
2019-09-02 00:14:38 +0900

    fix

--- src/actions/Application.ts
+++ src/actions/Application.ts
@@ -1,22 +1,32 @@
 import * as ActionType from './ApplicationConstants';
+import { User } from '../reducers/ApplicationReducer';
 
 interface SelectTaskListPrams {
   selectedTaskListId: string;
 }
 export const application = {
-  selectTaskList: (param: SelectTaskListPrams) => ({
-    type: ActionType.SELECT_TASKLIST,
-    payload: param,
+  selectTaskList: (paramForSelect: SelectTaskListPrams) => ({
+    type: ActionType.SELECT_TASKLIST as typeof ActionType.SELECT_TASKLIST,
+    payload: paramForSelect,
   }),
   initializeGapiClient: () => ({
-    type: ActionType.INITIALIZE_GAPI_CLIENT,
+    type: ActionType.INITIALIZE_GAPI_CLIENT as typeof ActionType.INITIALIZE_GAPI_CLIENT,
   }),
   signIn: () => ({
-    type: ActionType.SIGN_IN,
+    type: ActionType.SIGN_IN as typeof ActionType.SIGN_IN,
   }),
   signOut: () => ({
-    type: ActionType.SIGN_OUT,
+    type: ActionType.SIGN_OUT as typeof ActionType.SIGN_OUT,
+  }),
+  setUpUser: (user: User) => ({
+    type: ActionType.SETUP_USER as typeof ActionType.SETUP_USER,
+    playload: user,
   }),
 };
 
-export type ApplicationAction = ReturnType<typeof application.selectTaskList>;
+export type ApplicationAction =
+  | ReturnType<typeof application.selectTaskList>
+  | ReturnType<typeof application.initializeGapiClient>
+  | ReturnType<typeof application.signIn>
+  | ReturnType<typeof application.signOut>
+  | ReturnType<typeof application.setUpUser>;
