f9073fe312b90d4eb1f3b7ad573f537da5ea203f
nkgrnkgr <beefupme@gmail.com>
2019-09-05 23:25:57 +0900

    URLのクエリからタスクリストを選択できるように修正また、選択したタスクリストをURLに反映

--- src/containers/TaskListSelection/index.tsx
+++ src/containers/TaskListSelection/index.tsx
@@ -1,15 +1,19 @@
 import React, { FC, useEffect } from 'react';
 import { bindActionCreators, Dispatch } from 'redux';
 import { connect } from 'react-redux';
+import { RouteComponentProps } from 'react-router';
 import { TaskList } from '../../services/googleTasks/models';
 import { application } from '../../actions/Application';
 import { getTaskLists } from '../../actions/TaskList';
 import TaskListSelection from '../../components/TaskListSelection/index';
 import { CombinedState } from '../../reducers/root';
 
+type OuterProps = RouteComponentProps;
+
 interface StateProps {
   taskLists: TaskList[];
   isLoading?: boolean;
+  selectedTaskListId: string;
 }
 
 interface DispatchProps {
@@ -17,11 +21,12 @@ interface DispatchProps {
   selectTaskList: (selectedTaskListId: string) => void;
 }
 
-type EnhancemembersProps = StateProps & DispatchProps;
+type EnhancemembersProps = StateProps & DispatchProps & OuterProps;
 
 const mapStateTopProps = (state: CombinedState): StateProps => ({
   taskLists: state.taskList.taskLists,
   isLoading: state.taskList.isLoading,
+  selectedTaskListId: state.application.selectedTaskListId,
 });
 
 const mapDispatchToProps = (dispatch: Dispatch): DispatchProps =>
@@ -41,6 +46,8 @@ const TaskContainers: FC<EnhancemembersProps> = ({
   taskLists = [],
   isLoading = false,
   selectTaskList,
+  selectedTaskListId,
+  history,
 }) => {
   useEffect(() => {
     setTimeout(() => {
@@ -48,11 +55,17 @@ const TaskContainers: FC<EnhancemembersProps> = ({
     }, 1000);
   }, []);
 
+  const handleOnChange = (tasklistId: string) => {
+    selectTaskList(tasklistId);
+    history.push(`/?tasklist=${tasklistId}`);
+  };
+
   return (
     <TaskListSelection
       taskLists={taskLists}
       isLoading={isLoading}
-      handleOnChange={selectTaskList}
+      handleOnChange={handleOnChange}
+      defaultSelectedTaskListId={selectedTaskListId}
     />
   );
 };
