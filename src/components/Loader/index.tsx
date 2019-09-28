32bbf39ef70181f5b70e826ada6d3d178fdae917
nkgrnkgr <beefupme@gmail.com>
2019-09-28 19:47:41 +0900

    不要なモジュールを削除 & LoadingkからSemantic-UIを削除

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,12 +1,27 @@
 import React, { FC } from 'react';
-import { Loader } from 'semantic-ui-react';
+import {
+  CircularProgress,
+  makeStyles,
+  Theme,
+  createStyles,
+} from '@material-ui/core';
 
-interface LoadingProps {
-  isInverted?: boolean;
+interface Props {
+  color?: 'primary' | 'secondary' | 'inherit';
 }
 
-const Loading: FC<LoadingProps> = ({ isInverted = false }) => (
-  <Loader inline="centered" active inverted={isInverted} />
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    progress: {
+      margin: theme.spacing(2),
+    },
+  }),
 );
+
+const Loading: FC<Props> = ({ color = 'secondary' }) => {
+  const classes = useStyles();
+
+  return <CircularProgress className={classes.progress} color={color} />;
+};
 
 export default Loading;
